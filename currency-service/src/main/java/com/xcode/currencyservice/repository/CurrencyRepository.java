2730c1d1a4bd086101be99b9f0d7e0946b03ef9c
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-07-31 20:56:13 +0200

    Add currency repository

---
+++ currency-service/src/main/java/com/xcode/currencyservice/repository/CurrencyRepository.java
@@ -0,0 +1,43 @@
+package com.xcode.currencyservice.repository;
+
+import com.xcode.currencyservice.model.CurrencyHistory;
+import com.xcode.currencyservice.model.CurrencyRequest;
+import com.xcode.currencyservice.model.NbpApiResponse;
+import com.xcode.currencyservice.model.NbpCurrencyRate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+
+@Repository
+public class CurrencyRepository implements ICurrencyRepository{
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public CurrencyRepository(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public CurrencyHistory getCurrentCurrencyValue(CurrencyRequest currencyRequest) {
+        String url = "http://api.nbp.pl/api/exchangerates/tables/A?format=json";
+        NbpApiResponse[] response = restTemplate.getForObject(url, NbpApiResponse[].class);
+
+        // TODO: exception handling
+        if(response != null) {
+            for(NbpCurrencyRate rate : response[0].rates()) {
+                if(rate.code().equals(currencyRequest.currency())) {
+                    return new CurrencyHistory(
+                            currencyRequest.currency(),
+                            currencyRequest.name(),
+                            LocalDateTime.now(),
+                            rate.mid()
+                    );
+                }
+            }
+        }
+
+        return null;
+    }
+}
