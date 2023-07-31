309a3152a44dd0cd3880aca49a23bd11d90ab7a7
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-07-31 22:26:26 +0200

    Add user repository instance

--- currency-service/src/main/java/com/xcode/currencyservice/service/CurrencyService.java
+++ currency-service/src/main/java/com/xcode/currencyservice/service/CurrencyService.java
@@ -2,6 +2,7 @@ package com.xcode.currencyservice.service;
 
 import com.xcode.currencyservice.model.CurrencyRequest;
 import com.xcode.currencyservice.repository.CurrencyRepository;
+import com.xcode.currencyservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +10,12 @@ import org.springframework.stereotype.Service;
 public class CurrencyService {
 
     private final CurrencyRepository currencyRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public CurrencyService(CurrencyRepository currencyRepository) {
+    public CurrencyService(CurrencyRepository currencyRepository, UserRepository userRepository) {
         this.currencyRepository = currencyRepository;
+        this.userRepository = userRepository;
     }
 
     public Double getCurrentCurrencyValue(CurrencyRequest currencyRequest) {
