11c9ffca8d04d91e2ecba1709b53f26336bd9ee4
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-01 19:55:54 +0200

    Add equals and hashcode methods

--- currency-service/src/main/java/com/xcode/currencyservice/model/dto/UserEntryDTO.java
+++ currency-service/src/main/java/com/xcode/currencyservice/model/dto/UserEntryDTO.java
@@ -1,10 +1,30 @@
 package com.xcode.currencyservice.model.dto;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public record UserEntryDTO(
         String currency,
         String name,
         LocalDateTime date,
         double value
-) {}
+) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if(!(o instanceof UserEntryDTO))
+            return false;
+        UserEntryDTO other = (UserEntryDTO) o;
+
+        return Objects.equals(currency, other.currency) &&
+                Objects.equals(name, other.name) &&
+                Objects.equals(date, other.date) &&
+                Double.compare(other.value, value) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currency, name, date, value);
+    }
+}
