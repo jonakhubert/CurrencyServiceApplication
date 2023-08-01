97d894413681cea37584eaff1177dc8c9acde669
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-01 15:05:37 +0200

    Change location package

--- currency-service/src/main/java/com/xcode/currencyservice/model/api/NbpApiResponse.java
+++ currency-service/src/main/java/com/xcode/currencyservice/model/api/NbpApiResponse.java
@@ -1,4 +1,4 @@
-package com.xcode.currencyservice.model;
+package com.xcode.currencyservice.model.api;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
