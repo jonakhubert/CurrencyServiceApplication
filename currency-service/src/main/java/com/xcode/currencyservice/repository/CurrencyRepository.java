1b5194f8bffb42b268544f9d60c484be8eba47f7
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-01 15:07:20 +0200

    Remove redundant comments

--- currency-service/src/main/java/com/xcode/currencyservice/repository/CurrencyRepository.java
+++ currency-service/src/main/java/com/xcode/currencyservice/repository/CurrencyRepository.java
@@ -2,8 +2,8 @@ package com.xcode.currencyservice.repository;
 
 import com.xcode.currencyservice.model.UserEntry;
 import com.xcode.currencyservice.model.CurrencyRequest;
-import com.xcode.currencyservice.model.NbpApiResponse;
-import com.xcode.currencyservice.model.NbpCurrencyRate;
+import com.xcode.currencyservice.model.api.NbpApiResponse;
+import com.xcode.currencyservice.model.api.NbpCurrencyRate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
@@ -25,7 +25,6 @@ public class CurrencyRepository implements ICurrencyRepository {
         String url = "http://api.nbp.pl/api/exchangerates/tables/A?format=json";
         NbpApiResponse[] response = restTemplate.getForObject(url, NbpApiResponse[].class);
 
-        // TODO: exception handling
         if(response != null) {
             for(NbpCurrencyRate rate : response[0].rates()) {
                 if(rate.code().equals(currencyRequest.currency())) {
