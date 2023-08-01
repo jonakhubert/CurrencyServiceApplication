25e245bbe4ea06e6580afe3baa5e5cbf3b71f30d
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-01 15:04:24 +0200

    Replace user entry on dto object and handle exceptions

--- currency-service/src/main/java/com/xcode/currencyservice/service/CurrencyService.java
+++ currency-service/src/main/java/com/xcode/currencyservice/service/CurrencyService.java
@@ -1,34 +1,48 @@
 package com.xcode.currencyservice.service;
 
+import com.xcode.currencyservice.exception.model.CurrencyNotFoundException;
 import com.xcode.currencyservice.model.UserEntry;
 import com.xcode.currencyservice.model.CurrencyRequest;
 import com.xcode.currencyservice.model.CurrencyResponse;
+import com.xcode.currencyservice.model.dto.UserEntryDTO;
 import com.xcode.currencyservice.repository.CurrencyRepository;
 import com.xcode.currencyservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CurrencyService {
 
     private final CurrencyRepository currencyRepository;
     private final UserRepository userRepository;
+    private final UserEntryDTOMapper userEntryDTOMapper;
 
     @Autowired
-    public CurrencyService(CurrencyRepository currencyRepository, UserRepository userRepository) {
+    public CurrencyService(CurrencyRepository currencyRepository, UserRepository userRepository,
+                           UserEntryDTOMapper userEntryDTOMapper) {
         this.currencyRepository = currencyRepository;
         this.userRepository = userRepository;
+        this.userEntryDTOMapper = userEntryDTOMapper;
     }
 
     public CurrencyResponse getCurrentCurrencyValue(CurrencyRequest currencyRequest) {
-        UserEntry currencyHistory = currencyRepository.getCurrentCurrencyValue(currencyRequest);
-        userRepository.save(currencyHistory);
-        return new CurrencyResponse(currencyHistory.getValue());
+        UserEntry userEntry = currencyRepository.getCurrentCurrencyValue(currencyRequest);
+
+        if(userEntry == null) {
+            throw new CurrencyNotFoundException(currencyRequest.currency() + " currency not found.");
+        }
+
+        userRepository.save(userEntry);
+        return new CurrencyResponse(userEntry.getValue());
     }
 
-    public List<UserEntry> getAllEntries() {
-        return userRepository.findAll();
+    public List<UserEntryDTO> getAllEntries() {
+        return userRepository.findAll()
+                .stream()
+                .map(userEntryDTOMapper)
+                .collect(Collectors.toList());
     }
 }
