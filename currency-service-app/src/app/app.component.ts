be76b2a54d76070d979f5548aaf4b8d480d40814
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-02 15:23:55 +0200

    Refactor code

--- currency-service-app/src/app/app.component.ts
+++ currency-service-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from './service/currency.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'currency-service-app';
+  
+  constructor(private currencyService: CurrencyService) {}
 }
