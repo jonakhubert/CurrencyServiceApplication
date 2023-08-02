d67715b338069ff4dc09f2f4ca1d92515b217855
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-02 22:15:36 +0200

    Add routing for components

--- currency-service-app/src/app/app-routing.module.ts
+++ currency-service-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './component/form/form.component';
+import { DisplayDataComponent } from './component/display-data/display-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FormComponent},
+  {path: 'display-data', component: DisplayDataComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- currency-service-app/src/app/app.module.ts
+++ currency-service-app/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FormComponent } from './component/form/form.component';
+import { FormsModule } from '@angular/forms';
+import { DisplayDataComponent } from './component/display-data/display-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormComponent,
+    DisplayDataComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
