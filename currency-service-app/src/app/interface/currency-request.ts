f9c7b6a7769cac694af98505813a38e22edfce1e
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-02 17:49:16 +0200

    Remove angular project

--- currency-service-app/src/app/interface/currency-request.ts
+++
@@ -1,4 +0,0 @@
-export interface CurrencyRequest {
-    currency: string;
-    name: string;
-}
--- currency-service-app/src/app/interface/user-entry.ts
+++
@@ -1,6 +0,0 @@
-export interface UserEntry {
-    currency: string;
-    name: string;
-    date: Date;
-    value: number;
-}
