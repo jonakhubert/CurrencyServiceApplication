929edc76d1dbff5732a758480a38b0f179d5c0f3
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-02 22:13:26 +0200

    Add components for data displaying

---
+++ currency-service-app/src/app/component/display-data/display-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-data',
+  templateUrl: './display-data.component.html',
+  styleUrls: ['./display-data.component.scss']
+})
+export class DisplayDataComponent {
+
+}
---
+++ currency-service-app/src/app/component/form/form.component.ts
@@ -0,0 +1,50 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ApiError } from 'src/app/interface/api-error';
+import { CurrencyRequest } from 'src/app/interface/currency-request';
+import { CurrencyResponse } from 'src/app/interface/currency-response';
+import { CurrencyService } from 'src/app/service/currency.service';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+
+export class FormComponent implements OnInit {
+  currencyValue: number = 0;
+  message: string = "";
+
+  constructor(private currencyService: CurrencyService) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(formData: NgForm): void {
+    if(formData.invalid) {
+      this.currencyValue = 0;
+      alert("You have to provide currency and name.");
+      return;
+    }
+
+    const request: CurrencyRequest = {
+      currency: formData.value.currency,
+      name: formData.value.name
+    };
+
+    this.currencyService.getCurrentCurrencyValue(request).subscribe(
+      (response: CurrencyResponse) => {
+        this.currencyValue = response.value;
+        this.message = `${request.currency.toUpperCase()}: ${this.currencyValue}`;
+      },
+      (error: ApiError) => {
+        console.error('Error:', error);
+        if (error.statusCode === 404) {
+          this.message = `${error.message}`;
+        } else {
+          this.message = 'An unexpected error occurred. Please try again later.';
+        }
+      }
+    );
+  }
+}
